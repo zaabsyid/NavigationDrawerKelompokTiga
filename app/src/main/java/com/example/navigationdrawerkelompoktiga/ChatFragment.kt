dbb36acb752262518e767af09814797532323951
DanialFarros <danialfarrosmaulana921@gmail.com>
2022-09-05 02:17:14 +0700

    add chat telegram

---
+++ app/src/main/java/com/example/navigationdrawerkelompoktiga/ChatFragment.kt
@@ -0,0 +1,13 @@
+package com.example.navigationdrawerkelompoktiga
+
+class ChatFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false)
+    }
+
+}
