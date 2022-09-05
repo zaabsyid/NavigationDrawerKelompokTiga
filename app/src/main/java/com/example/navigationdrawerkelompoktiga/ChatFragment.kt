37d49350e32bc5c50f7f9b6d6bb1d5d002b2655b
aghniqisthi <20082010113@student.upnjatim.ac.id>
2022-09-05 13:10:47 +0700

    Commit Navigation Drawer Telegram

--- app/src/main/java/com/example/navigationdrawerkelompoktiga/ChatFragment.kt
+++ app/src/main/java/com/example/navigationdrawerkelompoktiga/ChatFragment.kt
@@ -1,7 +1,11 @@
 package com.example.navigationdrawerkelompoktiga
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class ChatFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
--- app/src/main/java/com/example/navigationdrawerkelompoktiga/MainActivity.kt
+++ app/src/main/java/com/example/navigationdrawerkelompoktiga/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.navigationdrawerkelompoktiga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var drawerLayoutTele:DrawerLayout = findViewById(R.id.drawerLayoutTelegram)
+
+        // drawer layout instance to toggle the menu icon to open drawer and back button to close drawer
+        var actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayoutTele, R.string.nav_open, R.string.nav_close)
+
+        // pass the Open and Close toggle for the drawer layout listener to toggle the button
+        drawerLayoutTele.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        // to make the Navigation drawer icon always appear on the action bar
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    // override the onOptionsItemSelected() function to implement the item click listener callback to open and close the navigation drawer when the icon is clicked
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var drawerLayoutTele:DrawerLayout = findViewById(R.id.drawerLayoutTelegram)
+        var actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayoutTele, R.string.nav_open, R.string.nav_close)
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) return true
+        else return super.onOptionsItemSelected(item)
     }
 }
